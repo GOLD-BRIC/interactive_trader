5aa4a96a5b0f13694b793d42fe824fbcec4347e0
Lucas <97921187+ls4351@users.noreply.github.com>
2022-04-19 02:11:05 -0400

    Server setup (#1)

---
+++ serve_ita.bat
@@ -0,0 +1,4 @@
+cd C:\Users\vcm\production\interactive_trader_app
+git pull https://github.com/GOLD-BRIC/interactive_trader.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
